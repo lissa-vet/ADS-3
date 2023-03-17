4a3265ad2ed6acb6acacf9e8ed8046ac6b40da12
lissa-vet <125082940+lissa-vet@users.noreply.github.com>
2023-03-17 19:11:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int left_border = 0;
+    int right_border = size - 1;
+    int count = 0;
+    int mid;
+    while (left_border != right_border) {
+        mid = (left_border + right_border) / 2;
+        if (arr[mid] == value)
+        {
+            count++;
+            int mid1 = mid - 1;
+            while (arr[mid1] == value) {
+                count++;
+                mid1--;
+            }
+            int mid2 = mid + 1;
+            while (arr[mid2] == value) {
+                count++;
+                mid2++;
+            }
+            left_border = right_border;
+        }
+        if (arr[mid] > value)
+            right_border = mid;
+        if (arr[mid] < value)
+            left_border = mid + 1;
+    }
+    return count;
 }
